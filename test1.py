f92f24b84bfdf028a07e30bbf6212354e5774f3d
nanax <yttan16@yahoo.com>
2015-10-08 19:48:27 +0800

    prog1
    solving a quadratic equation

---
+++ test1.py
@@ -0,0 +1,25 @@
+"""solving a quadratic equation"""
+from math import sqrt
+def my_input_number():
+	sign=True
+	while sign:
+		inum=raw_input()
+		try:
+			inum=float(inum)
+			sign=False
+		except:
+			print "not a correct number ,please type in again"
+	return inum
+print "Quadratic term coefficient"
+a=my_input_number()
+print "term coefficient"
+b=my_input_number()
+print "Constant term"
+c=my_input_number()
+if (b**2-4*a*c)>0:
+	print "x1="+str(-b+sqrt(b**2-4*a*c)/(2*a))
+	print "x2="+str(-b-sqrt(b**2-4*a*c)/(2*a))
+elif (b**2-4*a*c)==0:
+	print "x1=x2="+str(-b/(2*a))
+else:
+	print "this equation has no solution"
